b520c60229f99d343ce02c46a6ba296f75fbd7f7
violet-zct <chunting.violet.zhou@gmail.com>
2017-04-29 02:07:39 -0400

    11

--- get_vocab.sh
+++ get_vocab.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 python vocab.py \
     --train_src ./en-de/train.en-de.low.filt.de \
-    --train_tgt ./en-de/train.en-de.low.filt.en
+    --train_tgt ./en-de/train.en-de.low.filt.en \
+    --src_vocab_size 30000 \
+    --tgt_vocab_size 20000
